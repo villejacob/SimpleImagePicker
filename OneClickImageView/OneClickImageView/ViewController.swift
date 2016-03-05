caceee51d2ff766e6cdf740b17aa2ce97f8dbadb
Jacob Ville <jacob@roccobotics.com>
2016-03-05 16:09:12 -0600

    Add all files for OneClickImageView

---
+++ OneClickImageView/OneClickImageView/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  OneClickImageView
+//
+//  Created by Jacob Ville on 2/29/16.
+//  Copyright © 2016 Jacob Ville Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+    }
+
+    @IBAction func pickImage(sender: AnyObject) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    //Methods to complete UIImagePickerControllerDelegate
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[String: AnyObject]){
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .ScaleAspectFit
+            imageView.image = pickedImage
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
+
